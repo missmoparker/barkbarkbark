ed3af2d6d00f38a14017a0780427e6a142839a11
Morgan Parker <missmoparker@gmail.com>
2019-01-16 16:05:14 -0600

    Add files via upload

---
+++ MLPe1.java
@@ -0,0 +1,14 @@
+//hola this is a comment
+/*
+Morgan Parker
+CSCE 111
+1/16/19
+*/
+class MLPe1 {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println("Morgan Parker is awesome");
+
+
+  }// end main
+}// end class
